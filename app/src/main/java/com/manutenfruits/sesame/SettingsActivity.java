1720032bf220ddb36213aafd999317d7f552a558
manutenfruits <manutenfruits@gmail.com>
2015-08-28 01:42:36 +0200

    Added MainActivity and SettingsActivity

--- app/src/main/java/com/manutenfruits/sesame/SettingsActivity.java
+++ app/src/main/java/com/manutenfruits/sesame/SettingsActivity.java
@@ -1,24 +1,22 @@
 package com.manutenfruits.sesame;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class SettingsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
         return true;
     }
 
@@ -31,8 +29,7 @@ public class MainActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.settings_button) {
-            Intent settingsIntent = new Intent(this, SettingsActivity.class);
-            startActivity(settingsIntent);
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
